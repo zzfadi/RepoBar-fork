dc95f1a5d6dc974f70844e97d713ce864fac7889
Peter Steinberger <steipete@gmail.com>
2025-12-31 19:48:31 +0100

    style: reduce menu padding

--- Sources/RepoBar/Support/MenuStyle.swift
+++ Sources/RepoBar/Support/MenuStyle.swift
@@ -3,23 +3,23 @@ import SwiftUI
 enum MenuStyle {
     static let menuItemContainerTrailingPadding: CGFloat = 10
 
-    static let cardHorizontalPadding: CGFloat = 20
+    static let cardHorizontalPadding: CGFloat = 13
     static let cardVerticalPadding: CGFloat = 6
     static let cardSpacing: CGFloat = 6
-    static let cardSeparatorInset: CGFloat = 20
+    static let cardSeparatorInset: CGFloat = 13
     static let cardSeparatorVerticalPadding: CGFloat = 4
 
-    static let filterHorizontalPadding: CGFloat = 24
+    static let filterHorizontalPadding: CGFloat = 13
     static let filterVerticalPadding: CGFloat = 3
 
-    static let headerHorizontalPadding: CGFloat = 20
+    static let headerHorizontalPadding: CGFloat = 13
     static let headerTopPadding: CGFloat = 10
     static let headerBottomPadding: CGFloat = 6
 
-    static let sectionHorizontalPadding: CGFloat = 24
+    static let sectionHorizontalPadding: CGFloat = 13
     static let sectionVerticalPadding: CGFloat = 8
 
-    static let bannerHorizontalPadding: CGFloat = 20
+    static let bannerHorizontalPadding: CGFloat = 13
     static let bannerVerticalPadding: CGFloat = 4
 
     static let submenuIconColumnWidth: CGFloat = 18
